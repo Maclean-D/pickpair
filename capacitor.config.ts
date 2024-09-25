507fabc884121955d5204e80ca997bbe843498e8
Maclean Dunkin <65423362+Maclean-D@users.noreply.github.com>
2024-09-24 21:04:58 -0600

    Add Android support via capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.pickpair',
   appName: 'pickpair',
-  webDir: 'build'
+  webDir: 'out'
 };
 
 export default config;
